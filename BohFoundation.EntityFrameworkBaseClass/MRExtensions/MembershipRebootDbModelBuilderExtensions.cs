898c01197fb7071c405ef8dfc08cb1df501df6ba
Sobieck00 <thomas@sobieck.com>
2014-10-16 10:31:37 -0500

    Added minor change.

--- BohFoundation.EntityFrameworkBaseClass/MRExtensions/MembershipRebootDbModelBuilderExtensions.cs
+++ BohFoundation.EntityFrameworkBaseClass/MRExtensions/MembershipRebootDbModelBuilderExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿// From https://github.com/brockallen/BrockAllen.MembershipReboot
+
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data.Entity;
